8cf11713d99c804e3bad1f28303aa4d86040a80e
Allximik <allximik50@gmail.com>
2022-02-04 21:51:45 +0300

    horizon

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,6 +38,22 @@ class HomeController extends Controller
         'queueName' => 'smart_rub_queue',
         'skipSum'   => 500,
       ],
+      [
+        'stock'     => 'Exmo',
+        'diff'      => 1,
+        'maxTrade'  => 100,
+        'pair'      => 'MKR_DAI',
+        'queueName' => 'mkr_dai_queue',
+        'skipSum'   => 10,
+      ],
+      /*[
+        'stock'     => 'Exmo',
+        'diff'      => 1,
+        'maxTrade'  => 200,
+        'pair'      => 'BTC_PLN',
+        'queueName' => 'btc_pln_queue',
+        'skipSum'   => 20,
+      ],*/
     ];
 
     foreach($parameters as $parameter) {
